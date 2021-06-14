a5f8dea1e9afef73b8fd22bdfd822f46cabd515f
unknown <hayashi.yuhei@iecl.ntt.co.jp>
2021-06-14 18:23:14 +0900

    modified

--- program/main.py
+++ program/main.py
@@ -1,11 +1,11 @@
+import ipaddress
 
 class AdaptiveNetworkMonitoring:
-    network_name
-    current_router_models
-    candidate_router_models
 
-    def __init__(self, routers_csv):
-        return 0
+    def __init__(self, network_name, current_router_models, candidate_router_models):
+        self.network_name = network_name
+        self.current_router_models = current_router_models
+        self.candidate_router_models = candidate_router_models
     def currentize_router_models():
         return 0
     def show_current_netflow_monitoring_statuses():
@@ -26,17 +26,17 @@ class AdaptiveNetworkMonitoring:
         return 0
 
 class RouterModel:
-    router_name
-    router_ip
-    ssh_id
-    ssh_pass
-    netflow_exportermaps
-    netflow_monitormaps
-    netflow_samplermaps
-    interfaces
 
-    def __init__(self, router_name, router_ip, ssh_id, ssh_pass, netflow_exportermaps, netflow_monitormaps, netflow_samplermaps, interfaces)
-        return 0
+    def __init__(self, router_name, router_ip, ssh_id, ssh_pass, netflow_exportermaps, netflow_monitormaps, netflow_samplermaps, interfaces):
+        self.router_name = router_name
+        self.router_ip = router_ip
+        self.ssh_id = ssh_id
+        self.ssh_pass = ssh_pass
+        self.netflow_exportermaps = netflow_exportermaps
+        self.netflow_monitormaps = netflow_monitormaps
+        self.netflow_samplermaps = netflow_samplermaps
+        self.interfaces = interfaces
+
     def currentize_router_model():
         return 0
     def get_runnning_netflow_configuration():
@@ -55,29 +55,32 @@ class RouterModel:
         return 0
 
 class NetflowExporterMap:
-      netflow_exporter_name
-      collector_addr
-      collector_udp_port
 
     def __init__(self, netflow_exporter_name, collector_addr, collector_udp_port):
-        return 0
+        self.netflow_exporter_name = netflow_exporter_name
+        self.collector_addr = collector_addr
+        self.collector_udp_port = collector_udp_port
 
 class NetflowMonitorMap:
-      netflow_monitormap_name
-      num_chache
-      chache_timeout
-      netflowexportermap
 
-    def __init__(self, netflow_monitormap_name, num_cache, chache_timeout, netflowexportermap):
-        return 0
+    def __init__(self, netflow_monitormap_name, num_chache, chache_timeout, netflowexportermap):
+        self.netflow_monitormap_name = netflow_monitormap_name
+        self.num_chache = num_chache
+        self.chache_timeout = chache_timeout
+        self.netflowexportermap = netflowexportermap
 
-class NetflowMonitorMap:
-    interface_name
-    samplermap
-    netflow_monitormap
+class SamplerMap:
+
+    def __init__(self, samplermap_name, sampring_rate):
+        self.samplermap_name = samplermap_name
+        self.sampring_rate = sampring_rate
+
+class Interface:
 
     def __init__(self, interface_name, samplermap, netflow_monitormap):
-        return 0
+        self.interface_name =  interface_name
+        self.samplermap =  samplermap
+        self.netflow_monitormap = netflow_monitormap
 
 if __name__ == "__main__":
     print("hello world")
